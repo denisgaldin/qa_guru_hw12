fe6f43a1d3091e97d00c523f4b5b9c53f0c7c377
denisgaldin <denisgaldin@mail.ru>
2024-11-20 17:47:20 +0300

    изменение кода в папке test/conftest/

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,56 +1,38 @@
-import os
-
 import pytest
-from selenium import webdriver
+from selene import browser
 from selenium.webdriver.chrome.options import Options
-from selene import Browser, Config
-from dotenv import load_dotenv
-
+from selenium import webdriver
 from utils import attach
 
-DEFAULT_BROWSER_VERSION = "100.0"
 
+# добавляем фикстуры
 
-def pytest_addoption(parser):
-    parser.addoption(
-        '--browser_version',
-        default='100.0'
-    )
-
-
-@pytest.fixture(scope='session', autouse=True)
-def load_env():
-    load_dotenv()
-
-
-@pytest.fixture(scope='function')
-def setup_browser(request):
-    browser_version = request.config.getoption('--browser_version')
-    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
+@pytest.fixture(scope="function")
+def setup_browser():
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": browser_version,
+        "browserVersion": "100.0",
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
         }
     }
     options.capabilities.update(selenoid_capabilities)
-
-    login = os.getenv('LOGIN')
-    password = os.getenv('PASSWORD')
-
     driver = webdriver.Remote(
-        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
-        options=options
-    )
-    browser = Browser(Config(driver))
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options)
+    browser.config.driver = driver
 
-    yield browser
+    browser.config.driver_name = "chrome"
+    browser.config.window_height = 1080
+    browser.config.window_width = 1920
+
+    yield
 
     attach.add_html(browser)
     attach.add_screenshot(browser)
     attach.add_logs(browser)
     attach.add_video(browser)
+
     browser.quit()
